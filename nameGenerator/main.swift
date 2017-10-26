d5e886ce33516cd16cbfa8257f5700399b8f7ad5
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-10-26 06:50:32 -0400

    Initial Commit

---
+++ nameGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  nameGenerator
+//
+//  Created by Marty Hernandez Avedon on 10/26/17.
+//  Copyright © 2017 Marty's . All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
